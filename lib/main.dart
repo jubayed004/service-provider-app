65423144e6b1c63429d13c3e7d6536d93febe248
moham <mohammadjubayed.islam97@gmail.com>
2025-03-06 22:39:39 +0600

    Home Some Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:service_provider_app/core/app_routes/app_routes.dart';
 import 'package:service_provider_app/core/utils/app_colors/app_colors.dart';
-import 'package:service_provider_app/core/utils/app_images/app_images.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:device_preview/device_preview.dart';
 void main() {
   runApp(const MyApp());
 
